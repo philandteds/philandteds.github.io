3121465e81d3e742ee02e21132892ead35f01080
netzwelt-vikas <vikas.arora@vtnetzwelt.com>
2019-05-27 10:10:51 +0530

    Update mobile_menu.js

--- mobile_menu.js
+++ mobile_menu.js
@@ -1,7 +1,9 @@
 /* Use this JS file for the PT mobile menu scroll issue fix */
 console.log ('Start of mobile_menu.js');
-$('button.menu-bars').click(function(){
-  alert('hi');
-  $(body).toggleClass('toggleMenu');
-});
+$(document).ready(function(){
+    $('button.menu-bars').click(function(){
+    alert('hi');
+    $(body).toggleClass('toggleMenu');
+  });
+})
 console.log ('End of mobile_menu.js');
