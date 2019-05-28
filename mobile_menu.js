a22cba518f9315afa5249820eb9b6eff94ac6045
netzwelt-vikas <vikas.arora@vtnetzwelt.com>
2019-05-28 10:23:28 +0530

    Update mobile_menu.js

--- mobile_menu.js
+++ mobile_menu.js
@@ -2,7 +2,10 @@
 console.log ('Start of mobile_menu.js');
 jQuery(document).ready(function(){
     jQuery('button.menu-bars').click(function(){
-    jQuery('body.en-US').toggleClass('toggleMenu');
+    jQuery('body').toggleClass('toggleMenu');
   });
+   jQuery('body').click(function(){
+     jQuery(this).removeClass('toggleMenu');
+   });
 })
 console.log ('End of mobile_menu.js');
